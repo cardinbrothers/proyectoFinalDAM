d5e34626452d3b7b8c5bd376cfa16effa9694f50
Alessandro <alessandro.cardin@clarcat.com>
2019-05-17 12:32:00 +0200

    Añadimos los metodos para obtener todos los jugadores de una partida y obtener la clasificacion de una partida

---
+++ WebService/WebService_ProyectoDAM/WebService_ProyectoDAM/ApiEntities/potenciajugadorEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_ProyectoDAM.ApiEntities
+{
+    public class potenciajugadorEntity
+    {
+        public string nombreJugador { get; set; }
+
+        public int potenciaJugador { get; set; }
+    }
+}

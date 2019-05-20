fa868907a98067c1d0dc9d3bb6059ff1cc5fa355
Alessandro <alessandrocardin99@gmail.com>
2019-05-21 00:28:45 +0200

    Añadido el controlador para reclutamiento de tropas

---
+++ WebService/WebService_ProyectoDAM/WebService_ProyectoDAM/Controllers/ReclutamientoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService_ProyectoDAM.ApiEntities;
+using WebService_ProyectoDAM.Servicios;
+using Newtonsoft.Json;
+
+namespace WebService_ProyectoDAM.Controllers
+{
+    [RoutePrefix("api/Reclutamiento")]
+    public class ReclutamientoController : ApiController
+    {
+        servicioReclutamientoTropas servicio = new servicioReclutamientoTropas();
+
+        [Route("reclutarTropas")]
+        [HttpPost]
+        public string reclutarTropas(int idPueblo, int idTropa, int cantidad)
+        {
+            var json = JsonConvert.SerializeObject(servicio.reclutarTropas(idPueblo, idTropa, cantidad));
+
+            return json;
+        }
+
+        [Route("obtenerOrdenes")]
+        [HttpGet]
+        public string obtenerOrdenes(int idPueblo)
+        {
+            var json = JsonConvert.SerializeObject(servicio.obtenerOrdenesActivas(idPueblo));
+
+            return json;
+        }
+
+        [Route("cancelar")]
+        [HttpPost]
+        public int cancelar(int idOrden)
+        {
+            return servicio.cancelarReclutamiento(idOrden);
+        }
+
+    }
+}

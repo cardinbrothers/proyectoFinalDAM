8e560dae32ade289c2270622a54b0b1c2610e308
Alessandro <alessandrocardin99@gmail.com>
2019-05-22 00:28:39 +0200

    Añadida vista enviar mensaje

---
+++ WindowsFormsDAMapp/WindowsFormsDAMapp/formEnviarMensaje.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsDAMapp
+{
+    partial class formEnviarMensaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_reclutamiento = new System.Windows.Forms.Button();
+            this.btn_movimientos = new System.Windows.Forms.Button();
+            this.btn_mapa = new System.Windows.Forms.Button();
+            this.btn_mensajes = new System.Windows.Forms.Button();
+            this.btn_Clasificacion = new System.Windows.Forms.Button();
+            this.btn_visionGeneral = new System.Windows.Forms.Button();
+            this.tbx_asunto = new System.Windows.Forms.TextBox();
+            this.tbx_jugador = new System.Windows.Forms.TextBox();
+            this.lab_asunto = new System.Windows.Forms.Label();
+            this.lab_usuario = new System.Windows.Forms.Label();
+            this.tbx_contenidoMensaje = new System.Windows.Forms.TextBox();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_reclutamiento
+            // 
+            this.btn_reclutamiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reclutamiento.Location = new System.Drawing.Point(117, 12);
+            this.btn_reclutamiento.Name = "btn_reclutamiento";
+            this.btn_reclutamiento.Size = new System.Drawing.Size(101, 34);
+            this.btn_reclutamiento.TabIndex = 54;
+            this.btn_reclutamiento.Text = "Reclutamiento";
+            this.btn_reclutamiento.UseVisualStyleBackColor = true;
+            // 
+            // btn_movimientos
+            // 
+            this.btn_movimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_movimientos.Location = new System.Drawing.Point(224, 12);
+            this.btn_movimientos.Name = "btn_movimientos";
+            this.btn_movimientos.Size = new System.Drawing.Size(101, 34);
+            this.btn_movimientos.TabIndex = 53;
+            this.btn_movimientos.Text = "Movimientos";
+            this.btn_movimientos.UseVisualStyleBackColor = true;
+            // 
+            // btn_mapa
+            // 
+            this.btn_mapa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mapa.Location = new System.Drawing.Point(331, 12);
+            this.btn_mapa.Name = "btn_mapa";
+            this.btn_mapa.Size = new System.Drawing.Size(101, 34);
+            this.btn_mapa.TabIndex = 52;
+            this.btn_mapa.Text = "Mapa";
+            this.btn_mapa.UseVisualStyleBackColor = true;
+            // 
+            // btn_mensajes
+            // 
+            this.btn_mensajes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mensajes.Location = new System.Drawing.Point(545, 12);
+            this.btn_mensajes.Name = "btn_mensajes";
+            this.btn_mensajes.Size = new System.Drawing.Size(101, 34);
+            this.btn_mensajes.TabIndex = 51;
+            this.btn_mensajes.Text = "Mensajes";
+            this.btn_mensajes.UseVisualStyleBackColor = true;
+            // 
+            // btn_Clasificacion
+            // 
+            this.btn_Clasificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clasificacion.Location = new System.Drawing.Point(438, 12);
+            this.btn_Clasificacion.Name = "btn_Clasificacion";
+            this.btn_Clasificacion.Size = new System.Drawing.Size(101, 34);
+            this.btn_Clasificacion.TabIndex = 50;
+            this.btn_Clasificacion.Text = "Clasificación";
+            this.btn_Clasificacion.UseVisualStyleBackColor = true;
+            // 
+            // btn_visionGeneral
+            // 
+            this.btn_visionGeneral.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_visionGeneral.Location = new System.Drawing.Point(10, 12);
+            this.btn_visionGeneral.Name = "btn_visionGeneral";
+            this.btn_visionGeneral.Size = new System.Drawing.Size(101, 34);
+            this.btn_visionGeneral.TabIndex = 49;
+            this.btn_visionGeneral.Text = "Visión General";
+            this.btn_visionGeneral.UseVisualStyleBackColor = true;
+            // 
+            // tbx_asunto
+            // 
+            this.tbx_asunto.Location = new System.Drawing.Point(137, 106);
+            this.tbx_asunto.Name = "tbx_asunto";
+            this.tbx_asunto.Size = new System.Drawing.Size(216, 20);
+            this.tbx_asunto.TabIndex = 93;
+            // 
+            // tbx_jugador
+            // 
+            this.tbx_jugador.Location = new System.Drawing.Point(137, 70);
+            this.tbx_jugador.Name = "tbx_jugador";
+            this.tbx_jugador.Size = new System.Drawing.Size(216, 20);
+            this.tbx_jugador.TabIndex = 92;
+            // 
+            // lab_asunto
+            // 
+            this.lab_asunto.AutoSize = true;
+            this.lab_asunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_asunto.Location = new System.Drawing.Point(34, 107);
+            this.lab_asunto.Name = "lab_asunto";
+            this.lab_asunto.Size = new System.Drawing.Size(52, 16);
+            this.lab_asunto.TabIndex = 91;
+            this.lab_asunto.Text = "Asunto:";
+            // 
+            // lab_usuario
+            // 
+            this.lab_usuario.AutoSize = true;
+            this.lab_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_usuario.Location = new System.Drawing.Point(34, 71);
+            this.lab_usuario.Name = "lab_usuario";
+            this.lab_usuario.Size = new System.Drawing.Size(61, 16);
+            this.lab_usuario.TabIndex = 90;
+            this.lab_usuario.Text = "Jugador:";
+            // 
+            // tbx_contenidoMensaje
+            // 
+            this.tbx_contenidoMensaje.Location = new System.Drawing.Point(37, 150);
+            this.tbx_contenidoMensaje.Multiline = true;
+            this.tbx_contenidoMensaje.Name = "tbx_contenidoMensaje";
+            this.tbx_contenidoMensaje.Size = new System.Drawing.Size(573, 187);
+            this.tbx_contenidoMensaje.TabIndex = 94;
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_enviar.Location = new System.Drawing.Point(495, 349);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(101, 34);
+            this.btn_enviar.TabIndex = 95;
+            this.btn_enviar.Text = "Enviar Mensaje";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            // 
+            // formEnviarMensaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 395);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.tbx_contenidoMensaje);
+            this.Controls.Add(this.tbx_asunto);
+            this.Controls.Add(this.tbx_jugador);
+            this.Controls.Add(this.lab_asunto);
+            this.Controls.Add(this.lab_usuario);
+            this.Controls.Add(this.btn_reclutamiento);
+            this.Controls.Add(this.btn_movimientos);
+            this.Controls.Add(this.btn_mapa);
+            this.Controls.Add(this.btn_mensajes);
+            this.Controls.Add(this.btn_Clasificacion);
+            this.Controls.Add(this.btn_visionGeneral);
+            this.Name = "formEnviarMensaje";
+            this.Text = "Enviar Mensaje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_reclutamiento;
+        private System.Windows.Forms.Button btn_movimientos;
+        private System.Windows.Forms.Button btn_mapa;
+        private System.Windows.Forms.Button btn_mensajes;
+        private System.Windows.Forms.Button btn_Clasificacion;
+        private System.Windows.Forms.Button btn_visionGeneral;
+        private System.Windows.Forms.TextBox tbx_asunto;
+        private System.Windows.Forms.TextBox tbx_jugador;
+        private System.Windows.Forms.Label lab_asunto;
+        private System.Windows.Forms.Label lab_usuario;
+        private System.Windows.Forms.TextBox tbx_contenidoMensaje;
+        private System.Windows.Forms.Button btn_enviar;
+    }
+}

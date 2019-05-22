3bbf44fc4de6f520f09319fd8689d641a564cbbe
Alessandro <alessandro.cardin@clarcat.com>
2019-05-22 09:20:42 +0200

    Añadidas las entidades y reorganizado el proyecto con carpetas

---
+++ WindowsFormsDAMapp/WindowsFormsDAMapp/Entidades/infoPartidaEntity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_ProyectoDAM.ApiEntities
+{
+    public class infoPartidaEntity
+    {
+        public int id_Partida { get; set; }
+
+        public int velocidad { get; set; }
+
+        public TimeSpan duracion { get; set; }
+
+        public int limiteJugadores { get; set; }
+
+        public int limitePoblacion { get; set; }
+
+        public DateTime fechaInicio { get; set; }
+
+        public int jugadoresActivos { get; set; }
+
+        public string jugadorGanador { get; set; }
+
+
+    }
+}
---
+++ WindowsFormsDAMapp/WindowsFormsDAMapp/Entidades/movimientossEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService_ProyectoDAM.ApiEntities
+{
+    public class movimientossEntity
+    {
+        public int id_Movimiento { get; set; }
+
+        public int puebloOrigen { get; set; }
+
+        public int puebloDestino { get; set; }
+
+        public string tipoMovimiento { get; set; }
+
+        public DateTime horaLlegada { get; set; }
+
+        public TimeSpan duracion { get; set; }
+
+        public int arqueros { get; set; }
+
+        public int ballesteros { get; set; }
+
+        public int piqueros { get; set; }
+
+        public int caballeros { get; set; }
+
+        public int paladines { get; set; }
+
+        public int vencedor { get; set; }
+
+    }
+}
